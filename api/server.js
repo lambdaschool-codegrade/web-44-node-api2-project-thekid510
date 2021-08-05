038c5546d2266cc77fa64508fc79b8b831df19bf
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-08-04 21:24:04 -0700

    initial commit

--- api/server.js
+++ api/server.js
@@ -1,2 +1,14 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+
+const server = express();
+
+// server.use(express.json());
+server.get('/',(req, res, )=> {
+    res.send(`
+    <h2>Post API</h2>
+    <p>Welcome to the web 44 is da best!!!</p>
+    ` );
+});
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // require your server and launch it here
+const server = require('./api/server');
+
+server.listen(6000, ()=> {
+    console.log('\n * Server Running on http://localhost:6000 *\n')
+});
